87ba478abce3720a5c0ba8e47a2961d9b16adf7a
Tung <tunga3109@gmail.com>
2023-01-11 14:19:19 +0200

    static files

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class MainTemplateView(TemplateView):
+class BaseMixin:
+    context = {
+        'twitter': 'https://twitter.com',
+        'facebook': 'https://facebook.com',
+    }
+
+
+class MainTemplateView(TemplateView, BaseMixin):
     template_name = 'blog/index.html'
 
-
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['heading'] = 'MORTAL KOMBAT FANPAGE'
+        context['subheading'] = 'WELCOME TO EARTHREALM'
+        context.update(self.context)
+        return context
